2a7c8f49ed85e4c281466a2388cbd3e42ebe8e60
feasible <98857201+feasiblegfx@users.noreply.github.com>
2022-05-24 08:56:27 -0400

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,56 @@
+const track = document.querySelector('.carousel-track')
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button--right');
+const prevButton = document.querySelector('.carousel-button--left');
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// console.log(slideWidth);
+
+
+//arrange the slides next to one another
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px'
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+
+// when I click left, move slides to the left
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide')
+    const prevSlide = currentSlide.previousElementSibling;
+   
+    moveToSlide(track, currentSlide, prevSlide);
+
+});
+
+
+
+
+// when I click right, move slides to right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide')
+    const nextSlide = currentSlide.nextElementSibling;
+   
+    moveToSlide(track, currentSlide, nextSlide);
+
+
+})
+
+
+// when I click the nav indicators, move to that slide
+
+
+
+
+
